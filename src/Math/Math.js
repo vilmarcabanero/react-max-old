6af5b77bcf0b428d5ee055de2fa80482060bced7
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-04-04 06:33:15 +0800

    Added mathjax-react

---
+++ src/Math/Math.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {MathComponent} from 'mathjax-react'
+
+const area = () => {
+  //const area = String.raw`\int_0^1 x^2\ dx` //
+  //return <MathComponent text={String.raw`A = \pi r^2`} display={false}/>
+  const example = String.raw`\text{A} = \pi r^2`;
+  return <MathComponent tex={example} display={false} />
+}
+
+export const Integral = () => {
+  //const area = String.raw`\int_0^1 x^2\ dx` //
+  //return <MathComponent text={String.raw`A = \pi r^2`} display={false}/>
+  const example = String.raw`\int_{-\infty}^{\infty}e^{-x^2}\ dx`;
+  return <MathComponent tex={example} display={false} />
+}
+
+export let Math = (math) => {
+  return <MathComponent tex={math} display={false} />
+}
+
+export default area;
